7ef7f46d1c8bf69e4f16c071f70a1e40c091908e
Russell Poldrack <poldrack@gmail.com>
2021-06-19 08:03:35 -0700

    initial add

---
+++ dataset_analyses/mongo.py
@@ -0,0 +1,23 @@
+# mongo utils
+
+from pymongo import MongoClient
+import pymongo
+
+def get_db_collection():
+    client = MongoClient()
+    db = client.openneuro
+    return(db.datasets)
+
+def add_info_to_db(info):
+    collection = get_db_collection()
+    collection.insert_one(info)
+
+def ds_from_database(dsnum):
+    collection = get_db_collection()
+    return(collection.find_one({'dsnum': dsnum}))
+
+def delete_ds_from_db(dsnum):
+    collection = get_db_collection()
+    query = {'dsnum': dsnum}
+    d = collection.delete_many(query)
+    print(f"{d.deleted_count} documents deleted for {dsnum}!!")
